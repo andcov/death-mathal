26e6cb0cb03048435ef5b56057d7f4689b6ed1f9
andcov <andreicovaci123@gmail.com>
2023-05-30 18:30:03 +0300

    Add about page

---
+++ src/routes/About.svelte
@@ -0,0 +1,30 @@
+<script>
+    import Katex from "../Katex.svelte";
+</script>
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Abhaya+Libre:wght@600&family=New+Rocker&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</svelte:head>
+<span class="metal">Death </span><span class="math">Math</span><span class="metal">al</span>
+
+<p>...is a place where I create tools that help me teach math.</p>
+
+<p>More often than not, kids aren't afraid of math due to a lack of talent or intelligence, but due to a lack of exposure.
+As such, it can sometimes be helpful to simply churn through a bunch of simple exercises in order to create a solid base, 
+and a computer can create millions of these exercises.</p>
+
+
+<p>Coincidentally, I have also found that the kids that require these exercises often perceive math as something terrible, akin to death, hence the name.</p>
+
+<style>
+    .metal {
+        font-family: 'New Rocker', cursive;
+        font-size: 5em;
+    }
+    .math {
+        font-family: 'Abhaya Libre', serif;
+        font-size: 5em;
+    }
+</style>
